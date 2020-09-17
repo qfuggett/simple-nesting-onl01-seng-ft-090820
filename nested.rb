672720e17af1ccd975498c8ca10b744c4c8712e5
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 03:29:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,8 +89,8 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+return programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+return programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
 
 end
 
@@ -132,5 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+return programmer_hash[:dennis_ritchie][:languages] <<"Assembly"
 
 end
