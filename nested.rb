ac4c77f5bffb0ccd9670ff0e0cd22745bf9d5e81
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 03:30:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
         }
      }
 
-return programmer_hash[:dennis_ritchie][:languages] <<"Assembly"
+return programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
