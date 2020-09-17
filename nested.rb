45cbc051884417bedad03a6b7cc4951b58e6379b
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 03:40:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,7 @@ def adding_matz
 
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
-
+return programmer_hash
 end
 
 def changing_alan
