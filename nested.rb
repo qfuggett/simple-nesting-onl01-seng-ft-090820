2366f64d0a20970a5ba614fbfb9a7300c257269e
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 03:12:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
 
-return dennis_ritchies_language[:dennis_ritchie][:languages]     
+return programmer_hash[:dennis_ritchie][:languages]     
 
 end
 
